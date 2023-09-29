158b20ff7fe688a43e844740320ab2c26b028500
Olamide Usman <khalidusaola18@gmail.com>
2023-09-29 09:13:24 -0700

    Add Cup Converter Form

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,19 @@ kgConverter = (kg) =>{
      return `${kg} kilograms equals ${gram} grams`
 }
 
-Litre = (l) =>{
+litreConverter = (l) =>{
     const ml = l * 1000;
     return `${l} litres equals ${ml} mililitre`
+}
+
+cupConverter = (c) =>{
+    const ounces = c * 8;
+    return `${c} cups equals ${ounces} ounces`
+}
+
+pintConverter = (p) =>{
+    const cup = p * 2;
+    return `${p} pints equals ${cup} cups`
 }
 
 // gConverter(10)
@@ -15,6 +25,7 @@ Litre = (l) =>{
 
 // UI Logic
 $(document).ready(() =>{
+    // Kilogram-gram Converter
     $("#converter1").submit((event)=>{
         event.preventDefault();
 
@@ -25,13 +36,36 @@ $(document).ready(() =>{
 
     })
 
+    // Litre-milliliter Converter
     $("#converter2").submit((event)=>{
         event.preventDefault();
 
-        const ml = parseInt($("#litre").val());
+        const litre = parseInt($("#litre").val());
 
-        const litreValue = Litre(ml);
-        $("#message2").text(litreValue);
+        const mlValue = litreConverter(litre);
+        $("#message2").text(mlValue);
+
+    })
+
+    // Cup-Ounces Converter 
+    $("#converter3").submit((event)=>{
+        event.preventDefault();
+
+        const cup = parseInt($("#cup").val());
+
+        const ounceValue = cupConverter(cup);
+        $("#message3").text(ounceValue);
+
+    })
+
+    // Cup-Ounces Converter 
+    $("#converter3").submit((event)=>{
+        event.preventDefault();
+
+        const cup = parseInt($("#cup").val());
+
+        const ounceValue = cupConverter(cup);
+        $("#message3").text(ounceValue);
 
     })
 })
